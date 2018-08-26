73977c8799e0609efc71cf5d78bd5e79ab8c6a16
hongman <hhwit@126.com>
2018-08-26 17:38:04 +0800

    Initial commit

---
+++ app/src/main/java/com/hhwit/liaisonshome/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hhwit.liaisonshome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
